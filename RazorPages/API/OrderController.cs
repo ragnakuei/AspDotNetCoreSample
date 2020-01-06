df784f84fb6a6ff929737c9713b6b1cb74125c0e
Kuei Peng <ragnakuei@livemail.tw>
2020-01-06 15:49:58 +0800

    ok

---
+++ RazorPages/API/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLogic.Order;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorPages.API
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost, Route("list")]
+        public IActionResult List(PageInfo pageInfo)
+        {
+            var result = _orderService.GetOrderList(pageInfo.Current - 1, pageInfo.RowCount);
+            return Ok(new
+                      {
+                          current = pageInfo.Current,
+                          rowCount = pageInfo.RowCount,
+                          total = result.TotalCount,
+                          rows = result.Items,
+                      });
+        }
+    }
+
+    public class PageInfo
+    {
+        public int Current { get; set; } = 1;
+        public int RowCount { get; set; } = 10;
+    }
+}
