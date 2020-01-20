00d2545f9008504a5c6041925de7ccd32259a8e3
Kuei Peng <ragnakuei@livemail.tw>
2020-01-20 17:08:11 +0800

    add vue project

---
+++ Vue/Controllers/OptionsController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLogic.Options;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vue.Controllers
+{
+    [ApiController]
+    [Route("api/options")]
+    public class OptionsController : ControllerBase
+    {
+        private readonly IOptionsService _optionsService;
+
+        public OptionsController(IOptionsService optionsService)
+        {
+            _optionsService = optionsService;
+        }
+        
+        [HttpGet, Route("customers")]
+        public IActionResult Customers(string keyword)
+        {
+            var result = _optionsService.GetCustomers(keyword);
+            return Ok(result);
+        }
+    }
+}
---
+++ Vue/Startup.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Vue
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddBusinessLogicServices();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                                             name: "default",
+                                             pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseVueCli(npmScript: "serve");
+                }
+            });
+        }
+    }
+}
