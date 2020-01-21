045e73901e4e80cf5be68f28034e466bc99ed15f
Kuei Peng <ragnakuei@livemail.tw>
2020-01-21 15:31:20 +0800

    ok

---
+++ VueCliMiddleware/VueCliBuilder.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.NodeServices.Util;
+using Microsoft.AspNetCore.SpaServices;
+using Microsoft.AspNetCore.SpaServices.Prerendering;
+using Microsoft.AspNetCore.SpaServices.Util;
+
+namespace VueCliMiddleware
+{
+    /// <summary>
+    /// Provides an implementation of <see cref="ISpaPrerendererBuilder"/> that can build
+    /// an Angular application by invoking the Angular CLI.
+    /// </summary>
+    [Obsolete("Prerendering is no longer supported out of box")]
+    public class VueCliBuilder : ISpaPrerendererBuilder
+    {
+        private static TimeSpan RegexMatchTimeout = TimeSpan.FromSeconds(5); // This is a development-time only feature, so a very long timeout is fine
+
+        private readonly string _scriptName;
+
+        /// <summary>
+        /// Constructs an instance of <see cref="VueCliBuilder"/>.
+        /// </summary>
+        /// <param name="npmScript">The name of the script in your package.json file that builds the server-side bundle for your Angular application.</param>
+        public VueCliBuilder(string npmScript)
+        {
+            if (string.IsNullOrEmpty(npmScript))
+            {
+                throw new ArgumentException("Cannot be null or empty.", nameof(npmScript));
+            }
+
+            _scriptName = npmScript;
+        }
+
+        /// <inheritdoc />
+        public async Task Build(ISpaBuilder spaBuilder)
+        {
+            var pkgManagerCommand = spaBuilder.Options.PackageManagerCommand;
+            var sourcePath = spaBuilder.Options.SourcePath;
+            if (string.IsNullOrEmpty(sourcePath))
+            {
+                throw new InvalidOperationException($"To use {nameof(VueCliBuilder)}, you must supply a non-empty value for the {nameof(SpaOptions.SourcePath)} property of {nameof(SpaOptions)} when calling {nameof(SpaApplicationBuilderExtensions.UseSpa)}.");
+            }
+
+            var logger = LoggerFinder.GetOrCreateLogger(
+                spaBuilder.ApplicationBuilder,
+                nameof(VueCliBuilder));
+            var scriptRunner = new NodeScriptRunner(
+                sourcePath,
+                _scriptName,
+                "--watch",
+                null,
+                pkgManagerCommand);
+            scriptRunner.AttachToLogger(logger);
+
+            using (var stdOutReader = new EventedStreamStringReader(scriptRunner.StdOut))
+            using (var stdErrReader = new EventedStreamStringReader(scriptRunner.StdErr))
+            {
+                try
+                {
+                    await scriptRunner.StdOut.WaitForMatch(
+                        new Regex("Date", RegexOptions.None, RegexMatchTimeout));
+                }
+                catch (EndOfStreamException ex)
+                {
+                    throw new InvalidOperationException(
+                        $"The {pkgManagerCommand} script '{_scriptName}' exited without indicating success.\n" +
+                        $"Output was: {stdOutReader.ReadAsString()}\n" +
+                        $"Error output was: {stdErrReader.ReadAsString()}", ex);
+                }
+                catch (OperationCanceledException ex)
+                {
+                    throw new InvalidOperationException(
+                        $"The {pkgManagerCommand} script '{_scriptName}' timed out without indicating success. " +
+                        $"Output was: {stdOutReader.ReadAsString()}\n" +
+                        $"Error output was: {stdErrReader.ReadAsString()}", ex);
+                }
+            }
+        }
+    }
+}
