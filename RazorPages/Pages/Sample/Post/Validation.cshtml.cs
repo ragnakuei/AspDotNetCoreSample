0b082aa3f291fd0627a6cbbfa732941ebb4e801c
Kuei <ragnakuei@livemail.tw>
2020-03-08 22:57:22 +0800

    ok

---
+++ RazorPages/Pages/Sample/Post/Validation.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPages.Pages.Sample.Post
+{
+    public class Validation : PageModel
+    {
+        public ValidationModel ValidationModel { get; set; }
+        
+        public void OnPost(ValidationModel t)
+        {
+            ValidationModel = t;
+        }
+    }
+
+    public class ValidationModel
+    {
+        [Required]
+        [StringLength( 60, MinimumLength = 2)]
+        public string Name { get; set; }
+    }
+}
